36a45f6c8f6c2f11ed005f9b534e5c1dee48a287
Taani Maama <taani1maama@live.com>
2021-05-25 21:44:50 -0700

    Finished the first exercise.

--- exercise-1-chai/textUtil.js
+++ exercise-1-chai/textUtil.js
@@ -1,3 +1,17 @@
 const expect = require("chai").expect;
 
-expect(true).to.be.false;
+const titleCase = title => {
+  const words = title.split(" ");
+  const titleCasedWords = words.map(
+    word => word[0].toUpperCase() + word.substring(1)
+  );
+
+  return titleCasedWords.join(" ");
+};
+
+expect(titleCase("the great mouse detective")).to.be.a("string");
+expect(titleCase("a")).to.equal("A");
+expect(titleCase("vertigo")).to.equal("Vertigo");
+expect(titleCase("the great mouse detective")).to.equal(
+  "The Great Mouse Detective"
+);
