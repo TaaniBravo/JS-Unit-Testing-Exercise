1efb40a4544ede10a0ad4902150ddaee4f1e5406
Taani Maama <taani1maama@live.com>
2021-05-25 22:11:55 -0700

    Created some tests and then the logic for when a player misses the other players battleship.

---
+++ battleship-engine/test/main_test.js
@@ -0,0 +1,10 @@
+const expect = require("chai").expect;
+
+// Test suite
+describe("Mocha is working", () => {
+  // Test spec (unit test)
+  it("should run our tests using npm", () => {
+    // ok is an insertion method.
+    expect(true).to.be.ok;
+  });
+});
